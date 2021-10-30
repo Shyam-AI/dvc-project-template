86d74f7ab5402818232f866763ca42afe772820e
Shyam-AI <shyamdl2803@gmail.com>
2021-10-30 20:08:13 +0530

    updated setup and requirements

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ setup(
     name=SRC_REPO,
     version="0.0.1",
     author=AUTHOR_USER_NAME,
-    description="A small package for DVC",
+    description="A package for DVC NLP usecase",
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/Shyam-AI/dvc-nlp",
